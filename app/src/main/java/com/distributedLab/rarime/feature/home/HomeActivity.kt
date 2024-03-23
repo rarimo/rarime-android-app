bf5186520322f6f64dbce12906317a1ec2a50c21
ardier16 <ardier16@gmail.com>
2024-03-23 13:15:48 +0200

    Add Inter font

--- app/src/main/java/com/distributedLab/rarime/feature/home/HomeActivity.kt
+++ app/src/main/java/com/distributedLab/rarime/feature/home/HomeActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.distributedLab.rarime.ui.theme.AppTheme
