060b67cbcc64b74fd796f446fc7cb44ab3a35774
Maksym <ardier16@gmail.com>
2024-03-26 16:04:09 +0200

    UI components (#2)
    * Add button
    
    * Add text field
    
    * Add icon
    
    * Add icon preview
    
    * Add switch
    
    * Update cursor color
    
    * Extract base button
    
    * Rename components
    
    * Add field states

--- app/src/main/java/com/distributedLab/rarime/feature/home/HomeActivity.kt
+++ app/src/main/java/com/distributedLab/rarime/feature/home/HomeActivity.kt
@@ -4,23 +4,30 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.distributedLab.rarime.R
+import com.distributedLab.rarime.ui.base.ButtonSize
+import com.distributedLab.rarime.ui.components.AppIcon
+import com.distributedLab.rarime.ui.components.AppSwitch
+import com.distributedLab.rarime.ui.components.AppTextField
+import com.distributedLab.rarime.ui.components.PrimaryButton
+import com.distributedLab.rarime.ui.components.rememberAppSwitchState
+import com.distributedLab.rarime.ui.components.rememberAppTextFieldState
 import com.distributedLab.rarime.ui.theme.AppTheme
 import com.distributedLab.rarime.ui.theme.RarimeTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,12 +38,48 @@ class HomeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = RarimeTheme.colors.backgroundPrimary
-                ) {
-                    List()
-                }
+                AppScreen()
             }
+        }
+    }
+}
+
+@Composable
+fun AppScreen() {
+    val textFieldState = rememberAppTextFieldState("")
+    var switchState = rememberAppSwitchState()
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = RarimeTheme.colors.backgroundPrimary
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.padding(20.dp)
+        ) {
+            List()
+            AppTextField(
+                state = textFieldState,
+                label = "Text Field",
+                placeholder = "Enter some text",
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                AppSwitch(state = switchState)
+                Text(
+                    text = "Switch",
+                    modifier = Modifier.padding(8.dp, 0.dp),
+                    color = RarimeTheme.colors.textPrimary,
+                    style = RarimeTheme.typography.subtitle4
+                )
+            }
+            PrimaryButton(
+                modifier = Modifier.fillMaxWidth(),
+                size = ButtonSize.Large,
+                text = "Rarime Button",
+                leftIcon = R.drawable.ic_rarime,
+                onClick = {
+                    textFieldState.updateErrorMessage("Some error message")
+                })
         }
     }
 }
@@ -53,14 +96,13 @@ fun List(homeViewModel: HomeViewModel = viewModel()) {
 @Composable
 fun Greeting(item: String) {
     Row(verticalAlignment = Alignment.CenterVertically) {
-        Icon(
-            painter = painterResource(id = R.drawable.ic_rarime),
-            contentDescription = "",
-            modifier = Modifier.size(24.dp),
+        AppIcon(
+            id = R.drawable.ic_rarime,
+            size = 24.dp,
             tint = RarimeTheme.colors.textPrimary
         )
         Text(
-            text = "Hello, Rarime $item!",
+            text = item,
             modifier = Modifier.padding(4.dp, 0.dp),
             color = RarimeTheme.colors.textPrimary,
             style = RarimeTheme.typography.subtitle1
@@ -74,7 +116,14 @@ fun Greeting(item: String) {
 fun GreetingPreview() {
     AppTheme {
         Surface(color = RarimeTheme.colors.backgroundPrimary) {
-            Greeting(item = "user")
+            Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                Greeting(item = "user")
+                PrimaryButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Rarime Button",
+                    leftIcon = R.drawable.ic_rarime,
+                    onClick = { /*TODO*/ }) {}
+            }
         }
     }
 }
--- app/src/main/java/com/distributedLab/rarime/ui/components/RarimeCard.kt
+++
@@ -1,8 +0,0 @@
-package com.distributedLab.rarime.ui.components
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun Card() {
-
-}
