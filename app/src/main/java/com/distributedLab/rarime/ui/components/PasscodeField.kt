a9694499c88c36f8194d1fad8d2599f69a3e68aa
ardier16 <ardier16@gmail.com>
2024-04-03 16:20:38 +0300

    Add passcode keyboard

--- app/src/main/java/com/distributedLab/rarime/ui/components/PasscodeField.kt
+++ app/src/main/java/com/distributedLab/rarime/ui/components/PasscodeField.kt
@@ -9,16 +9,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.distributedLab.rarime.R
 import com.distributedLab.rarime.ui.theme.RarimeTheme
 
 @Composable
@@ -28,53 +30,31 @@ fun PasscodeField(
     state: AppTextFieldState = rememberAppTextFieldState(""),
     onFilled: () -> Unit = {}
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        BasicTextField(
-            modifier = modifier,
-            value = state.text,
-            onValueChange = {
-                if (it.length <= maxLength) {
-                    state.updateText(it)
-                    state.updateErrorMessage("")
-                }
-
-                if (it.length == maxLength) {
-                    onFilled()
-                }
-            },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.NumberPassword
-            )
-        ) {
-            Row {
-                repeat(maxLength) { index ->
-                    Box(
-                        modifier = Modifier.padding(16.dp)
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .width(16.dp)
-                                .height(16.dp)
-                                .background(
-                                    if (state.isError) {
-                                        RarimeTheme.colors.errorDark
-                                    } else if (index < state.text.length) {
-                                        RarimeTheme.colors.primaryMain
-                                    } else {
-                                        RarimeTheme.colors.componentPrimary
-                                    },
-                                    CircleShape
-                                )
-                        )
-                    }
-                }
-            }
+    fun handleValueChange(value: String) {
+        if (value.length <= maxLength) {
+            state.updateText(value)
+            state.updateErrorMessage("")
         }
-        if (state.isError) {
+
+        if (value.length == maxLength) {
+            onFilled()
+        }
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(120.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            CodeValue(
+                value = state.text,
+                maxLength = maxLength,
+                isError = state.isError
+            )
             Text(
                 text = state.errorMessage,
                 style = RarimeTheme.typography.caption2,
@@ -82,19 +62,111 @@ fun PasscodeField(
                 textAlign = TextAlign.Center
             )
         }
+        PasscodeKeyboard(
+            value = state.text,
+            onValueChange = { handleValueChange(it) }
+        )
+    }
+}
+
+@Composable
+private fun CodeValue(
+    value: String,
+    maxLength: Int,
+    isError: Boolean
+) {
+    Row {
+        repeat(maxLength) { index ->
+            Box(modifier = Modifier.padding(16.dp)) {
+                Box(
+                    modifier = Modifier
+                        .width(16.dp)
+                        .height(16.dp)
+                        .background(
+                            if (isError) {
+                                RarimeTheme.colors.errorDark
+                            } else if (index < value.length) {
+                                RarimeTheme.colors.primaryMain
+                            } else {
+                                RarimeTheme.colors.componentPrimary
+                            },
+                            CircleShape
+                        )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun PasscodeKeyboard(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    Column {
+        repeat(3) { rowIndex ->
+            Row {
+                repeat(3) { columnIndex ->
+                    val number = rowIndex * 3 + columnIndex + 1
+                    PasscodeKey(
+                        modifier = Modifier.weight(1f),
+                        onClick = { onValueChange(value + number) }
+                    ) {
+                        Text(
+                            text = number.toString(),
+                            style = RarimeTheme.typography.subtitle2,
+                        )
+                    }
+                }
+            }
+        }
+        Row {
+            Box(modifier = Modifier.weight(1f))
+            PasscodeKey(
+                modifier = Modifier.weight(1f),
+                onClick = { onValueChange(value + 0) }
+            ) {
+                Text(
+                    text = "0",
+                    style = RarimeTheme.typography.subtitle2,
+                )
+            }
+            PasscodeKey(
+                modifier = Modifier.weight(1f),
+                onClick = {
+                    if (value.isNotEmpty()) {
+                        onValueChange(value.dropLast(1))
+                    }
+                }
+            ) {
+                AppIcon(id = R.drawable.ic_backspace)
+            }
+        }
+    }
+}
+
+@Composable
+private fun PasscodeKey(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    content: @Composable () -> Unit,
+) {
+    Button(
+        modifier = modifier.height(64.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = RarimeTheme.colors.textPrimary
+        ),
+        onClick = onClick
+    ) {
+        content()
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun PasscodeFieldPreview() {
-    val passcodeState = rememberAppTextFieldState("")
-    val errorPasscodeState = rememberAppTextFieldState("", "Error message")
-
-    Column(
-        modifier = Modifier.padding(12.dp, 16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        PasscodeField(state = passcodeState)
-        PasscodeField(state = errorPasscodeState)
-    }
+    val passcodeState = rememberAppTextFieldState("12", "Error message")
+    PasscodeField(state = passcodeState)
 }
