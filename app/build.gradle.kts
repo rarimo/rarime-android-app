5a847f06a5d4d827feed07196e047d213d5fd8e2
ardier16 <ardier16@gmail.com>
2024-05-01 16:55:00 +0300

    Add AppCompatActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,4 +102,5 @@ dependencies {
     implementation("androidx.security:security-crypto:1.1.0-alpha03")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.biometric:biometric:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 }
