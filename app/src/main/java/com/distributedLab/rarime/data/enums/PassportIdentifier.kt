5bd5e539c9673e4bb925cef7861adcd1279d9160
ardier16 <ardier16@gmail.com>
2024-05-08 13:55:23 +0300

    Format date

--- app/src/main/java/com/distributedLab/rarime/data/enums/PassportIdentifier.kt
+++ app/src/main/java/com/distributedLab/rarime/data/enums/PassportIdentifier.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.distributedLab.rarime.R
 import com.distributedLab.rarime.modules.passport.models.EDocument
+import com.distributedLab.rarime.util.DateUtil
 
 enum class PassportIdentifier(val value: String, val order: Int) {
     NATIONALITY("nationality", 0),
@@ -30,8 +31,8 @@ fun PassportIdentifier.toLocalizedValue(passport: EDocument): String {
     return when (this) {
         PassportIdentifier.NATIONALITY -> passport.personDetails?.nationality ?: ""
         PassportIdentifier.DOCUMENT_ID -> passport.personDetails?.serialNumber ?: ""
-        PassportIdentifier.EXPIRY_DATE -> passport.personDetails?.expiryDate ?: ""
-        PassportIdentifier.BIRTH_DATE -> passport.personDetails?.birthDate ?: ""
+        PassportIdentifier.EXPIRY_DATE -> DateUtil.formatDateString(passport.personDetails?.expiryDate)
+        PassportIdentifier.BIRTH_DATE -> DateUtil.formatDateString(passport.personDetails?.birthDate)
     }
 }
 
