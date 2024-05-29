30f019ba618aa67b6c9c330253a132d05ebef91b
lukachi <lukachisama@gmail.com>
2024-05-29 18:27:19 +0300

    fix modifiers order for WalletTokenCard root component

--- app/src/main/java/com/distributedLab/rarime/modules/wallet/walletTokens/WalletTokenCard.kt
+++ app/src/main/java/com/distributedLab/rarime/modules/wallet/walletTokens/WalletTokenCard.kt
@@ -1,6 +1,7 @@
 package com.distributedLab.rarime.modules.wallet.walletTokens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,10 +12,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidthIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.distributedLab.rarime.BaseConfig
@@ -23,52 +26,45 @@ import com.distributedLab.rarime.ui.theme.RarimeTheme
 @Composable
 fun WalletTokenCard() {
     Row (
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+
         modifier = Modifier
+            .requiredWidthIn(min = 300.dp)
+            .clip(RoundedCornerShape(24.dp))
+            .border(1.dp, RarimeTheme.colors.baseWhite, RoundedCornerShape(24.dp))
             // TODO: colors.Container?
             .background(RarimeTheme.colors.baseWhite)
-            .requiredWidthIn(min = 300.dp)
             .padding(16.dp)
-            // TODO: clip self
-//            .clip(RoundedCornerShape(24.dp))
-//            .border(2.dp, RarimeTheme.colors.baseWhite, RoundedCornerShape(24.dp))
     ) {
         Row (
-            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Row (
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(12.dp)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(40.dp)
-                        .background(RarimeTheme.colors.baseBlack)
-                )
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .background(RarimeTheme.colors.baseBlack)
+            )
 
-                Text(
-                    text = "Total " + BaseConfig.DENOM,
-                    style = RarimeTheme.typography.body3,
-                    color = RarimeTheme.colors.textSecondary
-                )
-            }
+            Text(
+                text = "Total " + BaseConfig.DENOM,
+                style = RarimeTheme.typography.body3,
+                color = RarimeTheme.colors.textSecondary
+            )
+        }
 
-            // FIXME: hotfix?
-            Spacer(modifier = Modifier.width(4.dp))
-
-            Column {
-                Text(
-                    text = "3",
-                    color = RarimeTheme.colors.textPrimary,
-                    style = RarimeTheme.typography.subtitle4
-                )
-                Text(
-                    text = "---",
-                    color = RarimeTheme.colors.textSecondary,
-                    style = RarimeTheme.typography.caption3
-                )
-            }
+        Column {
+            Text(
+                text = "3",
+                color = RarimeTheme.colors.textPrimary,
+                style = RarimeTheme.typography.subtitle4
+            )
+            Text(
+                text = "---",
+                color = RarimeTheme.colors.textSecondary,
+                style = RarimeTheme.typography.caption3
+            )
         }
     }
 }
--- app/src/main/java/com/distributedLab/rarime/modules/wallet/walletTokens/WalletTokensList.kt
+++ app/src/main/java/com/distributedLab/rarime/modules/wallet/walletTokens/WalletTokensList.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -15,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.distributedLab.rarime.modules.wallet.WalletScreen
 import com.distributedLab.rarime.ui.components.StepIndicator
 import com.distributedLab.rarime.ui.theme.RarimeTheme
 
