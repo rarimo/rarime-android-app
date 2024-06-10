9d32ba138b5b339a85a42a640f682ebc3e50eea4
lukachi <lukachisama@gmail.com>
2024-06-10 19:41:01 +0300

    mark summy text

--- app/src/main/java/com/distributedLab/rarime/modules/home/components/no_passport/non_specific/AboutProgram.kt
+++ app/src/main/java/com/distributedLab/rarime/modules/home/components/no_passport/non_specific/AboutProgram.kt
@@ -27,6 +27,7 @@ fun AboutProgram(
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.spacedBy(32.dp)
     ) {
+        // TODO: implement real text
         Text(
             text = """
                 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using
