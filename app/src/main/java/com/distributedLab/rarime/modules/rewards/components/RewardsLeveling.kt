92ce275569ca2ea00193a32503b344b3f8392259
Phat <39623143+lukachi@users.noreply.github.com>
2024-06-19 18:53:47 +0300

    Feature/leveling (#52)
    * Define leveling items wip
    
    * configure indicators my current level wip
    
    * implement pager for level cards

--- app/src/main/java/com/distributedLab/rarime/modules/rewards/components/RewardsLeveling.kt
+++ app/src/main/java/com/distributedLab/rarime/modules/rewards/components/RewardsLeveling.kt
@@ -1,5 +1,6 @@
 package com.distributedLab.rarime.modules.rewards.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -12,9 +13,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,12 +32,15 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.distributedLab.rarime.R
+import com.distributedLab.rarime.api.points.models.PointsBalanceData
+import com.distributedLab.rarime.api.points.models.PointsBalanceDataAttributes
 import com.distributedLab.rarime.ui.components.AppIcon
 import com.distributedLab.rarime.ui.components.CardContainer
 import com.distributedLab.rarime.ui.components.HorizontalDivider
 import com.distributedLab.rarime.ui.components.StepIndicator
 import com.distributedLab.rarime.ui.components.UiLinearProgressBar
 import com.distributedLab.rarime.ui.theme.RarimeTheme
+import com.distributedLab.rarime.util.NumberUtil
 
 @Composable
 private fun StageIndicator(
@@ -75,8 +86,14 @@ private fun GradientDivider(
     )
 }
 
+data class LevelReward(
+    val title: String,
+    val subtitle: String,
+    val iconId: Int,
+)
+
 @Composable
-private fun RewardsItem () {
+private fun RewardsItem (levelReward: LevelReward) {
     Row (
         modifier = Modifier
             .fillMaxWidth()
@@ -94,18 +111,18 @@ private fun RewardsItem () {
                 .height(40.dp),
             contentAlignment = Alignment.Center
         ) {
-            AppIcon(id = R.drawable.ic_users, size = 20.dp)
+            AppIcon(id = levelReward.iconId, size = 20.dp)
         }
 
         Column (
             verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
             Text (
-                text = "X2 RMO Coins",
+                text = levelReward.title,
                 style = RarimeTheme.typography.subtitle4,
             )
             Text (
-                text = "Invite friends in to app",
+                text = levelReward.subtitle,
                 style = RarimeTheme.typography.body4,
                 color = RarimeTheme.colors.textSecondary,
             )
@@ -114,8 +131,117 @@ private fun RewardsItem () {
     }
 }
 
+data class RewardLevel(
+    val title: String,
+    val subtitle: String,
+    val logo: Int,
+
+    val amount: Double,
+    val minAmount: Double,
+    val maxAmount: Double?,
+    val rewards: List<LevelReward>,
+)
+
+val LEVELING: List<RewardLevel> = listOf(
+    RewardLevel(
+        title = "Level 1",
+        subtitle = "0-30 RMO Coins",
+        logo = R.drawable.reward_coin,
+
+        amount = 0.0,
+        minAmount = 0.0,
+        maxAmount = 30.0,
+        rewards = listOf(
+            LevelReward(
+                title = "X2 RMO Coins",
+                subtitle = "Invite friends in to app",
+                iconId = R.drawable.ic_users,
+            ),
+            LevelReward(
+                title = "X2 RMO Coins",
+                subtitle = "Invite friends in to app",
+                iconId = R.drawable.ic_users,
+            )
+        ),
+    ),
+    RewardLevel(
+        title = "Level 2",
+        subtitle = "3-50 RMO Coins",
+        logo = R.drawable.reward_coin,
+
+        amount = 0.0,
+        minAmount = 30.0,
+        maxAmount = 50.0,
+        rewards = listOf(
+            LevelReward(
+                title = "X2 RMO Coins",
+                subtitle = "Invite friends in to app",
+                iconId = R.drawable.ic_users,
+            ),
+            LevelReward(
+                title = "X2 RMO Coins",
+                subtitle = "Invite friends in to app",
+                iconId = R.drawable.ic_users,
+            ),
+            LevelReward(
+                title = "X2 RMO Coins",
+                subtitle = "Invite friends in to app",
+                iconId = R.drawable.ic_users,
+            )
+        ),
+    ),
+    RewardLevel(
+        title = "Level 3",
+        subtitle = "50-xxx RMO Coins",
+        logo = R.drawable.reward_coin,
+
+        amount = 0.0,
+        minAmount = 50.0,
+        maxAmount = null,
+        rewards = listOf(
+            LevelReward(
+                title = "X2 RMO Coins",
+                subtitle = "Invite friends in to app",
+                iconId = R.drawable.ic_users,
+            )
+        ),
+    ),
+)
+
+val INFINITY_STUB = 999999999999999.0
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun RewardsLeveling() {
+fun RewardsLeveling(pointsBalance: PointsBalanceData) {
+    val balance = pointsBalance.attributes.amount.toDouble()
+
+    val leveling = LEVELING.map {
+        // if user has pass current level
+        if (it.maxAmount != null && balance >= it.maxAmount) {
+            it.copy(
+                amount = it.maxAmount
+            )
+        // if user is in current level
+        } else if (balance > it.minAmount && (it.maxAmount == null || balance < it.maxAmount)) {
+            it.copy(
+                amount = pointsBalance.attributes.amount.toDouble()
+            )
+        } else { it }
+    }
+
+    var selectedLevelingCardId by remember { mutableStateOf(leveling.indexOf(
+        leveling.find { it.amount == balance }
+    )) }
+
+    val pagerState = rememberPagerState(
+        initialPage = selectedLevelingCardId,
+        pageCount = { leveling.size },
+        initialPageOffsetFraction = 0f,
+    )
+    LaunchedEffect(pagerState.currentPage) {
+        selectedLevelingCardId = pagerState.currentPage
+    }
+
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -136,89 +262,99 @@ fun RewardsLeveling() {
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            StageIndicator(number = 1, isActive = false)
+            leveling.forEachIndexed { idx, it ->
+                StageIndicator(number = idx + 1, isActive = selectedLevelingCardId == idx)
 
-            GradientDivider()
-
-            StageIndicator(number = 2, isActive = true)
-
-            GradientDivider()
-
-            StageIndicator(number = 3, isActive = false)
+                if (idx < leveling.size - 1) {
+                    GradientDivider()
+                }
+            }
         }
 
         Spacer(modifier = Modifier.height(26.dp))
 
         Column (
-            modifier = Modifier.padding(horizontal = 12.dp)
+            modifier = Modifier.padding(horizontal = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
-            CardContainer() {
-                Column (
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Row (
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween,
+            HorizontalPager(
+                state = pagerState,
+                pageSpacing = 12.dp
+            ) { idx ->
+                val level = leveling[idx]
+
+                CardContainer {
+                    Column (
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        modifier = Modifier.fillMaxWidth()
                     ) {
-                        Column (
-                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        Row (
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                        ) {
+                            Column (
+                                verticalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                Text (
+                                    text = level.title,
+                                    style = RarimeTheme.typography.h5,
+                                )
+                                Text (
+                                    text = level.subtitle,
+                                    style = RarimeTheme.typography.body3,
+                                    color = RarimeTheme.colors.textSecondary,
+                                )
+                            }
+
+                            Image(
+                                painter = painterResource(id = R.drawable.reward_coin),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .width(72.dp)
+                                    .height(72.dp)
+                            )
+                        }
+
+                        Row (
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
                         ) {
                             Text (
-                                text = "Level 2",
-                                style = RarimeTheme.typography.h5,
+                                text = NumberUtil.formatAmount(level.amount),
+                                style = RarimeTheme.typography.subtitle3,
+                                color = RarimeTheme.colors.textPrimary,
                             )
-                            Text (
-                                text = "Subtitle text here",
-                                style = RarimeTheme.typography.body3,
+                            Text(
+                                text = " / " + (level.maxAmount?.let { maxAmount -> NumberUtil.formatAmount(maxAmount) } ?: "∞"),
                                 color = RarimeTheme.colors.textSecondary,
                             )
                         }
 
-                        Image(
-                            painter = painterResource(id = R.drawable.reward_coin),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .width(72.dp)
-                                .height(72.dp)
+                        UiLinearProgressBar(
+                            percentage = NumberUtil.formatAmount((level.amount / (level.maxAmount ?: INFINITY_STUB) * 100) / 100.0).toFloat(),
+                            trackColors = listOf(
+                                RarimeTheme.colors.primaryMain,
+                                RarimeTheme.colors.primaryDark,
+                                RarimeTheme.colors.primaryDarker,
+                            ),
+                            backgroundModifier = Modifier
+                                .fillMaxWidth()
+                                .height(8.dp)
+                                .clip(RoundedCornerShape(100.dp)),
                         )
                     }
-
-                    Row (
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text (
-                            text = "110",
-                            style = RarimeTheme.typography.subtitle3,
-                            color = RarimeTheme.colors.textPrimary,
-                        )
-                        Text(
-                            text = "/300",
-                            color = RarimeTheme.colors.textSecondary,
-                        )
-                    }
-
-                    UiLinearProgressBar(
-                        percentage = 0.36f,
-                        trackColors = listOf(
-                            RarimeTheme.colors.primaryMain,
-                            RarimeTheme.colors.primaryDark,
-                            RarimeTheme.colors.primaryDarker,
-                        ),
-                        backgroundModifier = Modifier
-                            .fillMaxWidth()
-                            .height(8.dp)
-                            .clip(RoundedCornerShape(100.dp)),
-                    )
                 }
             }
         }
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        StepIndicator(itemsCount = 3, selectedIndex = 1)
+        StepIndicator(
+            itemsCount = leveling.size,
+            selectedIndex = selectedLevelingCardId,
+            updateSelectedIndex = { selectedLevelingCardId = it },
+        )
 
         Spacer(modifier = Modifier.height(24.dp))
 
@@ -240,8 +376,9 @@ fun RewardsLeveling() {
             Column (
                 verticalArrangement = Arrangement.spacedBy(12.dp),
             ) {
-                RewardsItem()
-                RewardsItem()
+                leveling[selectedLevelingCardId].rewards.forEach {
+                    RewardsItem(it)
+                }
             }
         }
 
@@ -256,6 +393,22 @@ fun RewardsLevelingPreview() {
             .fillMaxSize()
             .background(RarimeTheme.colors.baseBlack)
     ) {
-        RewardsLeveling()
+        RewardsLeveling(
+            pointsBalance = PointsBalanceData(
+                id = "",
+                type = "",
+
+                attributes = PointsBalanceDataAttributes(
+                    amount = 9,
+                    is_disabled = false,
+                    is_verified = true,
+                    created_at = 0,
+                    updated_at = 0,
+                    rank = 0,
+                    referral_codes = null,
+                    level = 1,
+                )
+            )
+        )
     }
 }
