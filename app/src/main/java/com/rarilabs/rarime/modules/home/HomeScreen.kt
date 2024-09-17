635be1fc74ad09e0edb23d93b500d19f5c8201f3
lukachi <lukachisama@gmail.com>
2024-09-17 14:47:48 +0300

    init fb client

--- app/src/main/java/com/rarilabs/rarime/modules/home/HomeScreen.kt
+++ app/src/main/java/com/rarilabs/rarime/modules/home/HomeScreen.kt
@@ -1,23 +1,52 @@
 package com.rarilabs.rarime.modules.home
 
+import android.Manifest
+import android.os.Build
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.firebase.messaging.FirebaseMessaging
 import com.rarilabs.rarime.modules.home.components.no_passport.HomeScreenNoPassportMain
 import com.rarilabs.rarime.modules.home.components.passport.HomeScreenPassportMain
 
 val LocalHomeViewModel = compositionLocalOf<HomeViewModel> { error("No HomeViewModel provided") }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun HomeScreen(
     navigate: (String) -> Unit,
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
     val isShowPassport by homeViewModel.isShowPassport.collectAsState()
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val notificationPermission = rememberPermissionState(
+            permission = Manifest.permission.POST_NOTIFICATIONS
+        )
+
+        LaunchedEffect(Unit) {
+            try {
+                if (!notificationPermission.status.isGranted) {
+                    Log.i("Not granted notification", "Nice")
+                    notificationPermission.launchPermissionRequest()
+
+                } else {
+                    Log.i("Already granted notification", "Nice")
+                }
+            } catch(e: Exception) {
+                Log.e(e.toString(), e.stackTraceToString(), e)
+            }
+        }
+    }
 
     CompositionLocalProvider(LocalHomeViewModel provides homeViewModel) {
         if (isShowPassport) {
