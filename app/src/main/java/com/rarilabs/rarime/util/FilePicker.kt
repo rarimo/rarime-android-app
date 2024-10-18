8f081e3606a9436e2fc484adf2a264594257b1db
apik <51800892+ApereLieZ@users.noreply.github.com>
2024-10-18 12:57:20 +0300

    add eDocument parsing from json, add Android Type EDocument parsing (#114)

---
+++ app/src/main/java/com/rarilabs/rarime/util/FilePicker.kt
@@ -0,0 +1,54 @@
+package com.rarilabs.rarime.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.result.contract.ActivityResultContract
+
+class GetCustomContents(
+    private val isMultiple: Boolean = false, //This input check if the select file option is multiple or not
+): ActivityResultContract<String, List<@JvmSuppressWildcards Uri>>() {
+
+    override fun createIntent(context: Context, input: String): Intent {
+        return Intent(Intent.ACTION_GET_CONTENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = input //The input option es the MIME Type that you need to use
+            putExtra(Intent.EXTRA_LOCAL_ONLY, true) //Return data on the local device
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, isMultiple) //If select one or more files
+                .addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
+                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): List<Uri> {
+        return intent.takeIf {
+            resultCode == Activity.RESULT_OK
+        }?.getClipDataUris() ?: emptyList()
+    }
+
+    internal companion object {
+
+        //Collect all Uris from files selected
+        internal fun Intent.getClipDataUris(): List<Uri> {
+            // Use a LinkedHashSet to maintain any ordering that may be
+            // present in the ClipData
+            val resultSet = LinkedHashSet<Uri>()
+            data?.let { data ->
+                resultSet.add(data)
+            }
+            val clipData = clipData
+            if (clipData == null && resultSet.isEmpty()) {
+                return emptyList()
+            } else if (clipData != null) {
+                for (i in 0 until clipData.itemCount) {
+                    val uri = clipData.getItemAt(i).uri
+                    if (uri != null) {
+                        resultSet.add(uri)
+                    }
+                }
+            }
+            return ArrayList(resultSet)
+        }
+    }
+}
