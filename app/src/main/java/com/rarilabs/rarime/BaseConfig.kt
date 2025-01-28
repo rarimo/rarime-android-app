affd16c1280bae97dead72f170682588b6f19c17
yivanov <apereliez1@gmail.com>
2025-01-28 15:09:45 +0200

    update link for downloading

--- app/src/main/java/com/rarilabs/rarime/BaseConfig.kt
+++ app/src/main/java/com/rarilabs/rarime/BaseConfig.kt
@@ -179,9 +179,8 @@ object TestNetConfig : IConfig {
     override val registerIdentity_11_256_3_3_576_240_1_864_5_264: String =
         "https://storage.googleapis.com/rarimo-store/passport-zk-circuits/v0.2.8/registerIdentity_11_256_3_3_576_240_1_864_5_264-download.zip"
 
-    override val registerIdentity_11_256_3_5_576_248_1_1808_5_296: String = ""
-    //TODO()
-
+    override val registerIdentity_11_256_3_5_576_248_1_1808_5_296: String =
+        "https://storage.googleapis.com/rarimo-store/passport-zk-circuits/v0.2.9/registerIdentity_11_256_3_5_576_248_1_1808_5_296-download.zip"
     override val registerIdentityLight160: String =
         "https://storage.googleapis.com/rarimo-store/passport-zk-circuits/v0.2.6-light/registerIdentityLight160-download.zip"
     override val registerIdentityLight224: String =
@@ -291,7 +290,7 @@ object MainnetConfig : IConfig {
         "https://storage.googleapis.com/rarimo-store/passport-zk-circuits/v0.2.8/registerIdentity_11_256_3_3_576_240_1_864_5_264-download.zip"
 
     override val registerIdentity_11_256_3_5_576_248_1_1808_5_296: String =
-        TODO()
+        "https://storage.googleapis.com/rarimo-store/passport-zk-circuits/v0.2.9/registerIdentity_11_256_3_5_576_248_1_1808_5_296-download.zip"
 
     override val registerIdentityLight160: String =
         "https://storage.googleapis.com/rarimo-store/passport-zk-circuits/v0.2.6-light/registerIdentityLight160-download.zip"
