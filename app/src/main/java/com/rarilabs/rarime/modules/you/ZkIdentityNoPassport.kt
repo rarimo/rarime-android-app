714a41d19cd58a23f64086cc96ed3ecaf655d11f
Mykyta Sydorenko <102351233+lilbonekit@users.noreply.github.com>
2025-03-20 17:34:13 +0200

    Removed unused composed (#142)
    Removed unused compose

--- app/src/main/java/com/rarilabs/rarime/modules/you/ZkIdentityNoPassport.kt
+++ app/src/main/java/com/rarilabs/rarime/modules/you/ZkIdentityNoPassport.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RadialGradient
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,9 +58,6 @@ fun ZkIdentityNoPassport(modifier: Modifier = Modifier, navigate: (String) -> Un
             //  TODO: Implement other sheets
         }
     }
-
-
-    RadialGradient()
 
     val identityItems = remember {
         listOf(
