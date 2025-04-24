1000c1525b5881aa412eac536d763acc2de6ae4a
lilbonekit <mykytasydorenko0@gmail.com>
2025-04-24 19:49:39 +0300

    Removed imports

--- app/src/main/java/com/rarilabs/rarime/modules/digitalLikeness/DigitalLikenessProcessing.kt
+++ app/src/main/java/com/rarilabs/rarime/modules/digitalLikeness/DigitalLikenessProcessing.kt
@@ -1,6 +1,5 @@
 package com.rarilabs.rarime.modules.digitalLikeness
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -26,7 +25,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
