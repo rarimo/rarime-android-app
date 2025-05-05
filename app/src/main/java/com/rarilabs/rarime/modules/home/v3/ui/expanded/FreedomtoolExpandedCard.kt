bfac1d953f166b47e910b8e413d5ef68d2d95ea1
lilbonekit <mykytasydorenko0@gmail.com>
2025-05-05 21:05:38 +0300

    Removed unused import

--- app/src/main/java/com/rarilabs/rarime/modules/home/v3/ui/expanded/FreedomtoolExpandedCard.kt
+++ app/src/main/java/com/rarilabs/rarime/modules/home/v3/ui/expanded/FreedomtoolExpandedCard.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.hilt.navigation.compose.hiltViewModel
