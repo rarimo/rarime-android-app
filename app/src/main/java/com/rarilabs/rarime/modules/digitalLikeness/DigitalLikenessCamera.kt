a7b9d3b7999b9828f1440bf251691e0f63ffcd15
lilbonekit <mykytasydorenko0@gmail.com>
2025-05-05 11:13:52 +0300

    Removed unused imports

--- app/src/main/java/com/rarilabs/rarime/modules/digitalLikeness/DigitalLikenessCamera.kt
+++ app/src/main/java/com/rarilabs/rarime/modules/digitalLikeness/DigitalLikenessCamera.kt
@@ -21,11 +21,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,7 +32,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
