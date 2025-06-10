02a21925beb5da5d629491a01b63d4498db0ffde
Bogdan Guba <guba.bogd@gmail.com>
2025-06-10 13:38:11 +0300

    hide likeness and redact widget position

--- app/src/main/java/com/rarilabs/rarime/modules/home/v3/model/WidgetType.kt
+++ app/src/main/java/com/rarilabs/rarime/modules/home/v3/model/WidgetType.kt
@@ -3,10 +3,7 @@ package com.rarilabs.rarime.modules.home.v3.model
 enum class WidgetType(val layoutId: Int) {
     EARN(0),
     FREEDOMTOOL(1),
-
-    //IDENTITY(1),
-    //CLAIM(3),
     HIDDEN_PRIZE(2),
     RECOVERY_METHOD(3),
-    LIKENESS(4),
+    //LIKENESS(4),
 }
