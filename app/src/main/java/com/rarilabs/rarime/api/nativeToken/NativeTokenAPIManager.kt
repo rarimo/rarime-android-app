8ca9df1cfb2e6b4ca415c176c6542817c726032d
Bogdan Guba <guba.bogd@gmail.com>
2025-06-20 13:20:16 +0300

    add error handling

--- app/src/main/java/com/rarilabs/rarime/api/nativeToken/NativeTokenAPIManager.kt
+++ app/src/main/java/com/rarilabs/rarime/api/nativeToken/NativeTokenAPIManager.kt
@@ -2,6 +2,7 @@ package com.rarilabs.rarime.api.nativeToken.models
 
 import com.rarilabs.rarime.api.nativeToken.NativeTokenAPI
 import com.rarilabs.rarime.modules.wallet.models.Transaction
+import com.rarilabs.rarime.util.ErrorHandler
 import javax.inject.Inject
 
 class NativeTokenAPIManager @Inject constructor(
@@ -9,15 +10,38 @@ class NativeTokenAPIManager @Inject constructor(
 ) {
     suspend fun getAddressTransactions(walletAddress: String): List<Transaction> {
         val result = nativeTokenAPI.getTransactions(walletAddress)
-        return if (result.isSuccessful) {
-            result.body()?.items?.map {
-                TransactionItem.toTransaction(
-                    entity = it,
-                    walletAddress = walletAddress
-                )
-            } ?: emptyList()
-        } else {
-            emptyList()
+        when (result.code()) {
+            in 200..299 -> {
+                return result.body()?.items?.map {
+                    TransactionItem.toTransaction(
+                        entity = it,
+                        walletAddress = walletAddress
+                    )
+                } ?: emptyList()
+            }
+
+            404 -> {
+                ErrorHandler.logDebug("NativeTokenApi", "User has no transaction")
+                return emptyList()
+            }
+
+            422 -> {
+                ErrorHandler.logError("NativeTokenApi", "Invalid address")
+                return emptyList()
+            }
+
+            in 500..599 -> {
+                ErrorHandler.logError("NativeTokenApi", "Server is down")
+                return emptyList()
+            }
+
+            else -> {
+                ErrorHandler.logError("NativeTokenApi", "Something went wrong")
+                return emptyList()
+            }
+
         }
+
     }
+
 }
