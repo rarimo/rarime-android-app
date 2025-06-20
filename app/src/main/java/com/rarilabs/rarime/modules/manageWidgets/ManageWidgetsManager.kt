c975aa6c75963a31d404a2b368004d98655c8a32
yivanov <apereliez1@gmail.com>
2025-06-20 14:44:05 +0300

    fix crush on earn screen

--- app/src/main/java/com/rarilabs/rarime/modules/manageWidgets/ManageWidgetsManager.kt
+++ app/src/main/java/com/rarilabs/rarime/modules/manageWidgets/ManageWidgetsManager.kt
@@ -1,7 +1,7 @@
 package com.rarilabs.rarime.modules.manageWidgets
 
-import com.rarilabs.rarime.api.points.models.PointsBalanceBody
-import com.rarilabs.rarime.manager.PointsManager
+import android.util.Log
+import com.rarilabs.rarime.manager.WalletManager
 import com.rarilabs.rarime.modules.home.v3.model.WidgetType
 import com.rarilabs.rarime.store.SecureSharedPrefsManager
 import kotlinx.coroutines.CoroutineScope
@@ -10,6 +10,8 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -17,9 +19,11 @@ import javax.inject.Singleton
 @Singleton
 class ManageWidgetsManager @Inject constructor(
     private val sharedPrefsManager: SecureSharedPrefsManager,
-    private val pointsManager: PointsManager
+    private val walletManager: WalletManager
 ) {
     private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    private val pointsToken = walletManager.pointsToken
 
     private val _visibleWidgets = MutableStateFlow<List<WidgetType>>(emptyList())
     val visibleWidgets: StateFlow<List<WidgetType>>
@@ -36,24 +40,37 @@ class ManageWidgetsManager @Inject constructor(
         MutableStateFlow(allManagedWidgets.filter { it != WidgetType.HIDDEN_PRIZE }).asStateFlow()
 
     init {
+        initializeVisibleWidgets()
         scope.launch {
-            initializeVisibleWidgets()
+            pointsToken.filter { it?.balanceDetails?.attributes?.amount != null && it.balanceDetails?.attributes?.amount != 0L }
+                .distinctUntilChanged().collect {
+                    Log.i("initializeVisibleWidgets", "SOSAL?")
+                    initializeVisibleWidgets()
+                }
         }
     }
 
-    private suspend fun initializeVisibleWidgets() {
+    private fun initializeVisibleWidgets() {
         val visibleCardsStored = sharedPrefsManager.readVisibleWidgets()
 
         if (visibleCardsStored.isNullOrEmpty()) {
             val defaultWidgets = mutableListOf(WidgetType.RECOVERY_METHOD)
-            val pointBalance: PointsBalanceBody? = pointsManager.getPointsBalance()
+            val pointBalance = pointsToken.value?.balanceDetails
 
-            if (pointBalance != null && pointBalance.data.attributes.amount != 0L) {
+
+            if (pointBalance != null && pointBalance.attributes.amount != 0L) {
                 defaultWidgets.add(WidgetType.EARN)
             }
             setVisibleWidgets(defaultWidgets)
         } else {
-            _visibleWidgets.value = visibleCardsStored.filter { it != WidgetType.HIDDEN_PRIZE }
+            if (pointsToken.value?.balanceDetails?.attributes?.amount != null && pointsToken.value?.balanceDetails?.attributes?.amount != 0L) {
+                val tempCards = visibleCardsStored.toMutableList()
+                tempCards.add(WidgetType.EARN)
+                setVisibleWidgets(tempCards.filter { it != WidgetType.HIDDEN_PRIZE })
+                return
+            }
+
+            setVisibleWidgets(visibleCardsStored.filter { it != WidgetType.HIDDEN_PRIZE })
         }
     }
 
